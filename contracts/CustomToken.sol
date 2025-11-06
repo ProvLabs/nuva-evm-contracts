a448fa0c1c70b334e426adbace64ea5335328353
Tarun Jaiswal <tarunjais28@gmail.com>
2025-11-06 14:45:15 +0530

    fixed: linter

--- contracts/CustomToken.sol
+++ contracts/CustomToken.sol
@@ -6,12 +6,32 @@ import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC2
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-/// @title Custom ERC20 Token
+/**
+ * @title Custom ERC20 Token
+ * @notice Minimal ERC20 token with mint and burn functionality.
+ * @author NU Blockchain Technologies
+ */
 contract CustomToken is ERC20, ERC20Burnable, ERC20Permit, AccessControl {
+    /**
+     * @notice Role for minting tokens.
+     */
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    /**
+     * @notice Role for burning tokens.
+     */
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    /**
+     * @notice Custom decimals for the token.
+     */
     uint8 private _customDecimals;
 
+    /**
+     * @notice Initializes the contract with the provided token name, symbol, admin, and decimals.
+     * @param _name The name of the token.
+     * @param _symbol The symbol of the token.
+     * @param _admin The address of the admin.
+     * @param _decimals The number of decimals for the token.
+     */
     constructor(
         string memory _name,
         string memory _symbol,
@@ -25,14 +45,28 @@ contract CustomToken is ERC20, ERC20Burnable, ERC20Permit, AccessControl {
         _mint(_admin, 0);
     }
 
+    /**
+     * @notice Returns the number of decimals used by the token.
+     * @return The number of decimals.
+     */
     function decimals() public view override returns (uint8) {
         return _customDecimals;
     }
 
+    /**
+     * @notice Mints a specified amount of tokens to a specified address.
+     * @param to The address to which the tokens will be minted.
+     * @param amount The amount of tokens to mint.
+     */
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
+    /**
+     * @notice Burns a specified amount of tokens from a specified address.
+     * @param from The address from which the tokens will be burned.
+     * @param amount The amount of tokens to burn.
+     */
     function burnAuthorized(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
         _burn(from, amount);
     }
